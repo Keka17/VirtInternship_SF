4f0401365a92f44d56b8d30d8a9659c9fd3c483e
Victoria <vihalfblood@gmail.com>
2025-02-19 23:47:32 +0300

    Update database.py
    Добавлен класс Database для работы с БД

--- app/database.py
+++ app/database.py
@@ -22,3 +22,31 @@ engine = create_engine(DATABASE_URL)
 # Создание сессии БД для взаимодействия с ней
 SessionLocal = sessionmaker(bind=engine)
 
+# Создание базового класса для определения всех моделей
+Base = declarative_base()
+
+
+# Модель перевалов
+class PerevalAdded(Base):
+    __tablename__ = 'pereval_added'
+
+    id = Column(Integer, primary_key=True, index=True)
+    coord_id = Column(Integer, ForeignKey('coords_id'), nullable=False)
+    status = Column(String, default='new')
+
+
+# Класс для работы с БД
+class Database:
+    def __init__(self):
+        self.session = SessionLocal()
+
+        def add_pereval(self, coord_id: int):
+            """Логика добавление нового перевала в БД"""
+            new_pereval = PerevalAdded(coord_id=coord_id)
+            self.session.add(new_pereval)
+            self.session.commit()
+            return new_pereval.id
+
+        def close(self):
+            """Закрытие сесси после создания новой записи"""
+            self.session.close()
